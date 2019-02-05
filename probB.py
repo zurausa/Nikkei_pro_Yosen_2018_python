c581302880dfd0a2dba760f6d372311f5df8ea3a
TakuyaIto <zurausa@zurausa-mac.local>
2019-02-04 18:33:39 -0800

    change B

--- probB.py
+++ probB.py
@@ -1,17 +1,12 @@
 import sys
- 
+
 n=int(sys.stdin.readline())
-a=sys.stdin.readline()
-b=sys.stdin.readline()
-c=sys.stdin.readline()
- 
- 
+a=list(sys.stdin.readline())
+b=list(sys.stdin.readline())
+c=list(sys.stdin.readline())
+
+
 cnt = 0
 for i in range(n):
-    if a[i] == b[i] == c[i]:
-        continue
-    elif a[i] == b[i] or b[i] == c[i] or c[i] == a[i]:
-        cnt = cnt + 1
-    else:
-        cnt = cnt + 2
+    cnt += len(set([a[i],b[i],c[i]]))-1
 print(cnt)
